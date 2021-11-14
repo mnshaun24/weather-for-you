c18d26b6a532bef40735308c53300f7c92cf5e81
Shaun S <shaun.sticka@outlook.com>
2021-11-14 14:05:04 -0600

    fixed the fetch request and processing the returned data

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var myKey = "51133e26b6dba1c42e2e1b1a94f55fa2";
 // create global variables
 
 var city;
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + "Minneapolis" + "&appid=" + myKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "Minneapolis" + "&appid=" + myKey;
 var citySearchEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-entry");
 
@@ -19,10 +19,15 @@ var forecastSubmitHandler = function(event) {
 // add function to get the forecast
 
 var getForecast = function() {
-    var response = fetch(queryURL);
-    console.log(response);
+    var response = fetch(queryURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+});
 };
 
 // make an event listener function when someone submits a city name
 
 citySearchEl.addEventListener("submit", forecastSubmitHandler);
+
+getForecast();

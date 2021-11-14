2e6c48b4991bc2b1b001953446d2f6e990847f5d
Shaun S <shaun.sticka@outlook.com>
2021-11-13 23:14:41 -0600

    added API key and set up initial API variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
+// create variable for API Key
+
+var myKey = "51133e26b6dba1c42e2e1b1a94f55fa2";
+
 // create global variables
 
+var city;
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + "Minneapolis" + "&appid=" + myKey;
 var citySearchEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-entry");
 
@@ -8,6 +14,13 @@ var cityInputEl = document.querySelector("#city-entry");
 var forecastSubmitHandler = function(event) {
     event.preventDefault();
     console.log(event);
+};
+
+// add function to get the forecast
+
+var getForecast = function() {
+    var response = fetch(queryURL);
+    console.log(response);
 };
 
 // make an event listener function when someone submits a city name
